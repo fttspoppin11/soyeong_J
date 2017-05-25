766504e6e4809848556bfd9fb1ffe684211abb68
soyeongj <fttspoppin11@naver.com>
2017-05-25 19:51:57 +0900

    깃허브 첫사용 테스트

---
+++ src/Quiz_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Quiz_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String s ="hi welcom to the soyoung's house. 나의 첫 깃허브!";
+		
+		System.out.println(s);
+	
+	}
+
+}
